e9b668a7aca881cf05328a96a891b0335d8c7da7
matiasbarcelo <152202648+matiasbarcelo@users.noreply.github.com>
2023-11-29 13:10:42 -0600

    Update PlaylistNode.cpp

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -1,1 +1,29 @@
+#include "PlaylistNode.h"
 
+   PlaylistNode::PlaylistNode(){
+      uniqueID = "";
+      songName = "";
+      artistName = "";
+      songLength = 0;
+      nextNodePtr = nullptr;
+   }
+   
+   string PlaylistNode::GetID(){return uniqueID;}
+   string PlaylistNode::GetSongName(){return songName;}
+   string PlaylistNode::GetArtistName(){return artistName;}
+   int PlaylistNode::GetSongLength(){return songLength;}
+   PlaylistNode* PlaylistNode::GetNext(){return nextNodePtr;}
+   void PlaylistNode::InsertAfter(PlaylistNode* nodePtr){
+      PlaylistNode* temp = nextNodePtr;
+      nextNodePtr->SetNext(nodePtr);
+      nodePtr->SetNext(temp);
+   }
+   void PlaylistNode::SetNext(PlaylistNode* nodePtr){
+      nextNodePtr = nodePtr;
+   }
+   // Outputs uniqueID, songname, artistName, and songLength
+   void PlaylistNode::PrintPlaylistNode(){
+      cout << "Song Name: " << songName << endl;
+      cout << "Artist Name: " << artistName << endl;
+      cout << "Song Length (in seconds): " << songLength << endl;
+   }
