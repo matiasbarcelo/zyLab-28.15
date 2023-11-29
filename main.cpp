80dc9a442dac2510d362bf30537cb3d436d2e541
matiasbarcelo <152202648+matiasbarcelo@users.noreply.github.com>
2023-11-29 13:10:17 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 
 #include <iostream>
 #include "PlaylistNode.h"
-
 using namespace std;
 
 void PrintMenu(const string playlistTitle) {
-   /* Type your code here */
-   
+   cout << playlistTitle << " PLAYLIST MENU\na - Add song\nd - Remove song\nc - Change position of song\ns - Output songs by specific artist\nt - Output total time of playlist (in seconds)\no - Output full playlist\nq - Quit" << endl;
 }
 
 PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
-   /* Type your code here */
-   
+      
 }
 
 int main() {
-   /* Type your code here */
+   string playlistTitle;
+   
+   cout << "Enter playlist's title: " << endl;
+   cin >> playlistTitle;
+   
+   PrintMenu(playlistTitle);
    
    return 0;
 }
+
