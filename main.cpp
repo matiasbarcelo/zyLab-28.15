f591d41d4eaa3199f62bb18566ebc02bd1877931
matiasbarcelo <152202648+matiasbarcelo@users.noreply.github.com>
2023-11-29 12:09:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
+
 #include <iostream>
+#include "PlaylistNode.h"
+
 using namespace std;
+
+void PrintMenu(const string playlistTitle) {
+   /* Type your code here */
+   
+}
+
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   /* Type your code here */
+   
+}
+
+int main() {
+   /* Type your code here */
+   
+   return 0;
+}
